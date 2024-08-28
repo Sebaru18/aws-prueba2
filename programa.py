6a1eee06e087c0ff984507f904fef01054e72c48
Sebaru18 <Sebastian.ruiz03@usa.edu.co>
2024-08-28 05:02:36 +0000

    list of buckets functionality S3, 2.0

--- programa.py
+++ programa.py
@@ -1,4 +1,11 @@
-import boto3
-client = boto3.client("s3","us-east-1")
-response = client.list_buckets()
-print(response)
+import boto3 
+
+client= boto3.client("s3","us-east-1") 
+
+response = client.list_buckets() 
+
+print("Buckets en tu cuenta de S3:")  
+
+for bucket in response['Buckets']:  
+
+    print(f"- {bucket['Name']}")
